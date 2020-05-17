adf0b7972f0f3e0e2b05290b97aa7b6ee2c35cbf
Kukan.S <65235607+kukan-007@users.noreply.github.com>
2020-05-17 22:22:59 +0530

    Update cachematrix.R
    Code done with explanation

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL                             ## initailize inv to NULL which holds Inverse of a Matrix passed
+    set <- function(y) {                    ## define the set function to assign new 
+        x <<- y                             ## value of matrix in parent environment
+        inv <<- NULL                        ## if already new matrix is present, Reset to NULL
+    }
+        
+    get <- function() x                     ## define the get fucntion that returns the value of the matrix arguement
+
+    setinverse <- function(inverse) inv <<- inverse  ## assigns value for invin parent environment 
+    getinverse <- function() inv                     ## gets value when called 
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+                                                                                  ## to the functions with the $ operator
 
 }
 
@@ -11,5 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {                         # Check inverse matrix is NULL or not
+        message("getting cached data")
+        return(inv)                             # Gets invertible Matrix
+    }
+    data <- x$get()                             # gets original Data Matrix
+    inv <- solve(data, ...)                     # Solving the Matrix to get inverse
+    x$setinverse(inv)
+    inv                                         # returns invertible matrix
 }
